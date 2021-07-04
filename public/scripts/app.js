b3e29094c0e60cc12c74921dd2d002d2be0207e4
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-04 20:08:39 +0100

    add: src with app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,13 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>;
-var template = /*#__PURE__*/React.createElement("p", null, "This is JSX from app.js!");
+var template = React.createElement(
+  'h1',
+  null,
+  'Does this change?'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
