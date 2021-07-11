c2f45a35065624dd4403f31edb5ee2b18017cf39
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-11 18:52:18 +0100

    add: implement a button to choose which option the user should follow

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,6 +25,12 @@ var onRemoveAll = function onRemoveAll() {
 	renderApp();
 };
 
+var onMakeDecision = function onMakeDecision() {
+	var randomNum = Math.floor(Math.random() * app.options.length);
+	var option = app.options[randomNum];
+	alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var numbers = [55, 101, 1000];
@@ -49,9 +55,9 @@ var renderApp = function renderApp() {
 			app.options.length > 0 ? 'Here are your options' : 'No options'
 		),
 		React.createElement(
-			'p',
-			null,
-			app.options.length
+			'button',
+			{ disabled: app.options.length === 0, onClick: onMakeDecision },
+			'What should I do?'
 		),
 		React.createElement(
 			'button',
