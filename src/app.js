0f8c99c0b19b083d66880a68fdd16f1812d74770
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-12 19:55:53 +0100

    add: implement nesting and props for components

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+class IndecisionApp extends React.Component {
+	render() {
+		const title = 'Indecision';
+		const subtitle = 'Put your life in the hands of a computer';
+		const options = ['Thing one', 'Thing two', 'Thing four'];
+
+		return (
+			<div>
+				<Header title={title} subtitle={subtitle} />
+				<Action />
+				<Options options={options} />
+				<AddOption />
+			</div>
+		);
+	}
+}
+
 class Header extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Indecision</h1>
-				<h2>Put your life in the hands of a computer</h2>
+				<h1>{this.props.title}</h1>
+				<h2>{this.props.subtitle}</h2>
 			</div>
 		);
 	}
@@ -23,7 +40,19 @@ class Options extends React.Component {
 	render() {
 		return (
 			<div>
-				<p>Options component here</p>
+				{
+					this.props.options.map((option) => <Option key={option} optionText={option} />)
+				}
+			</div>
+		)
+	};
+}
+
+class Option extends React.Component {
+	render() {
+		return (
+			<div>
+				{this.props.optionText}
 			</div>
 		)
 	};
@@ -32,18 +61,11 @@ class Options extends React.Component {
 class AddOption extends React.Component {
 	render() {
 		return (
-			<p>AddOption component here</p>
+			<div>
+				<p>AddOption component here</p>
+			</div>
 		);
 	}
 }
 
-const jsx = (
-	<div>
-		<Header />
-		<Action />
-		<Options />
-		<AddOption />
-	</div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
