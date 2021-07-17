530b279cd698d989731175328aa2710bf930876c
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-17 17:45:09 +0100

    modify: use default prop values in app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
 		this.handlePick = this.handlePick.bind(this);
 		this.handleAddOption = this.handleAddOption.bind(this);
 		this.state = {
-			options: []
+			options: props.options
 		};
 	}
 	handleDeleteOptions() {
@@ -34,12 +34,11 @@ class IndecisionApp extends React.Component {
 		});
 	}
 	render() {
-		const title = 'Indecision';
 		const subtitle = 'Put your life in the hands of a computer';
 
 		return (
 			<div>
-				<Header title={title} subtitle={subtitle} />
+				<Header subtitle={subtitle} />
 				<Action
 					hasOptions={this.state.options.length > 0}
 					handlePick={this.handlePick}
@@ -56,14 +55,22 @@ class IndecisionApp extends React.Component {
 	}
 }
 
+IndecisionApp.defaultProps = {
+	options: []
+};
+
 const Header = (props) => {
 	return (
 		<div>
 			<h1>{props.title}</h1>
-			<h2>{props.subtitle}</h2>
+			{props.subtitle && <h2>{props.subtitle}</h2>}
 		</div>
 	);
 };
+
+Header.defaultProps = {
+	title: 'Indecision!'
+}
 
 const Action = (props) => {
 	return (
@@ -129,14 +136,5 @@ class AddOption extends React.Component {
 		);
 	}
 }
-
-// const User = (props) => {
-// 	return (
-// 		<div>
-// 			<p>Name: {props.name}</p>
-// 			<p>Age: {props.age}</p>
-// 		</div>
-// 	);
-// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
