f1e3533a5e5244dd5b007fa643a9d20b1768d711
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-21 11:45:11 +0100

    add: import react npm modules and set up babel config file to run jsx

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 20));
-
-import isSenior, { isAdult, canDrink } from "./person.js";
-
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(64));
+const template = <p>Testing 123</p>;
+ReactDOM.render(template, document.getElementById('app'));
