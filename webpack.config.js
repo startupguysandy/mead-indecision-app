77fcb90e9174494180db2b8ff21aa4c10419e335
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-25 20:26:39 +0100

    add: use devtool and devServer from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,9 @@ module.exports = {
 			test: /\.js$/,
 			exclude: /node_modules/
 		}]
+	},
+	devtool: 'eval-cheap-module-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'public')
 	}
 };
